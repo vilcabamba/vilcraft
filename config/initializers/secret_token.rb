ead65f606065ad0bbd72fae82ecbb728c71706c6
Mario Andrés Correa <admin@macool.me>
2014-01-30 14:20:13 -0500

    app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vilcraft::Application.config.secret_key_base = 'dc4c21ca3ac6dee67846e0a46fb85dddc0759a0493408fcd51693ff12c56ccc2a05ee4924a4f696dadcc42c68008aa6b86311e1f8d3cc22a3e3dc6f2d5ab5407'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vilcraft::Application.config.session_store :cookie_store, key: '_vilcraft_session'
